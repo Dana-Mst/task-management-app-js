a5e96636e91b9ad85721cb7334b307ad91750493
Dana-Mst <danactt27@gmail.com>
2023-06-02 12:15:03 +0300

    remove class form-input form select element clean form-input fields added remove comments

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ let taskList = [
     state: "not_started",
     blockReason: "",
     priority: 1,
-  },  
+  },
   {
     id: "3",
     title: "Training Python",
@@ -34,7 +34,8 @@ let taskList = [
     state: "progress",
     blockReason: "",
     priority: 1,
-  },   {
+  },
+  {
     id: "7",
     title: "Training React",
     description: "Learn how to use Ajax to send data to the server",
@@ -44,14 +45,14 @@ let taskList = [
     priority: 1,
   },
   {
-    id:"6",
+    id: "6",
     title: "Training Angular",
     description: "Learn how to use Ajax to send data to the server",
     assignees: ["Ioana", "Maria", "Sebastian"],
     state: "finished",
     blockReason: "",
     priority: 1,
-  }
+  },
 ];
 
 let assignees = ["Ioana", "Maria", "Sebastian", "Julian", "Uhtred"];
@@ -59,111 +60,112 @@ let assignees = ["Ioana", "Maria", "Sebastian", "Julian", "Uhtred"];
 document.addEventListener("DOMContentLoaded", onLoad);
 
 function onLoad() {
-    for(let task of taskList) {
-        addTask(task);
-    }
-    showForm();
-      
-    let inputFields = document.getElementsByClassName("form-input");
-    for(let inputField of inputFields) {
-        inputField.addEventListener("onFocus", clearInputValue);
-    };
-    let addTaskForm = document.getElementById("form-container");
-    addTaskForm.addEventListener("submit", onFormSubmit); 
+  for (let task of taskList) {
+    addTask(task);
+  }
+  showForm();
 
-    let pileDiv = document.getElementsByClassName("pile-div");
-    for(let pile of pileDiv) {
-        pile.addEventListener("dragover", allowDropTask);
-        pile.addEventListener("drop", dropTask );
-    }
+  let inputFields = document.getElementsByClassName("form-input");
+  for (let inputField of inputFields) {
+    inputField.addEventListener("onFocus", clearInputValue);
+  }
+  let addTaskForm = document.getElementById("form-container");
+  addTaskForm.addEventListener("submit", onFormSubmit);
 
-
+  let pileDiv = document.getElementsByClassName("pile-div");
+  for (let pile of pileDiv) {
+    pile.addEventListener("dragover", allowDropTask);
+    pile.addEventListener("drop", dropTask);
+  }
 }
 
-function addTask(task){
-    let taskDiv = document.createElement("div");
-    taskDiv.classList.add("task");
-    taskDiv.setAttribute("draggable", "true");
-    taskDiv.setAttribute("id", task.id);
-    taskDiv.addEventListener("dragstart", dragTask);
+function addTask(task) {
+  let taskDiv = document.createElement("div");
+  taskDiv.classList.add("task");
+  taskDiv.setAttribute("draggable", "true");
+  taskDiv.setAttribute("id", task.id);
+  taskDiv.addEventListener("dragstart", dragTask);
 
-    taskDiv.classList.add(task.state);
-    let taskTitle = document.createElement("p");
-    taskTitle.innerHTML = task.title;
-    let taskDescription = document.createElement("p");
-    taskDescription.innerHTML = task.description;
-    let taskAssignees = document.createElement("p");
-    taskAssignees.innerHTML = task.assignees.join(", ");
-    taskDiv.append(taskTitle, taskDescription, taskAssignees);
-    let pileDiv = document.getElementById(task.state);
-    pileDiv.append(taskDiv);
-    
+  taskDiv.classList.add(task.state);
+  let taskTitle = document.createElement("p");
+  taskTitle.innerHTML = task.title;
+  let taskDescription = document.createElement("p");
+  taskDescription.innerHTML = task.description;
+  let taskAssignees = document.createElement("p");
+  taskAssignees.innerHTML = task.assignees.join(", ");
+  taskDiv.append(taskTitle, taskDescription, taskAssignees);
+  let pileDiv = document.getElementById(task.state);
+  pileDiv.append(taskDiv);
 }
 
 function showForm() {
-    let formContainer = document.getElementById("form-container");
-    let titleInput = document.createElement("input");
-    titleInput.setAttribute("id", "form-title");
-    titleInput.setAttribute("type", "text");
-    titleInput.setAttribute("placeholder", "Title");
-    titleInput.classList.add("form-input");
-    formContainer.appendChild(titleInput);
+  let formContainer = document.getElementById("form-container");
+  let titleInput = document.createElement("input");
+  titleInput.setAttribute("id", "form-title");
+  titleInput.setAttribute("type", "text");
+  titleInput.setAttribute("placeholder", "Title");
+  titleInput.classList.add("form-input");
+  formContainer.appendChild(titleInput);
 
-    let descriptionInput = document.createElement("input");
-    descriptionInput.setAttribute("id", "form-description");
-    descriptionInput.setAttribute("type", "text");
-    descriptionInput.setAttribute("placeholder", "Description");  
-    descriptionInput.classList.add("form-input");
-    formContainer.appendChild(descriptionInput);
+  let descriptionInput = document.createElement("input");
+  descriptionInput.setAttribute("id", "form-description");
+  descriptionInput.setAttribute("type", "text");
+  descriptionInput.setAttribute("placeholder", "Description");
+  descriptionInput.classList.add("form-input");
+  formContainer.appendChild(descriptionInput);
 
-    let assigneesSelect = document.createElement("select");
-    assigneesSelect.setAttribute("id", "form-assignees");
-    assigneesSelect.setAttribute("placeholder", "Assignee");
-    assigneesSelect.setAttribute("multiple", true)
-    assigneesSelect.classList.add("form-input");
-    formContainer.appendChild(assigneesSelect);
-    for(let assignee of assignees) {
-      let option = document.createElement("option");
-      option.value = assignee;
-      option.innerHTML = assignee;
-      assigneesSelect.appendChild(option);
-    }
-
-    let addButton = document.createElement("button");
-    addButton.innerHTML = "Add";
-    addButton.setAttribute("type", "submit");
-    addButton.setAttribute("id", "add-btn");
-    formContainer.appendChild(addButton);
+  let assigneesSelect = document.createElement("select");
+  assigneesSelect.setAttribute("id", "form-assignees");
+  assigneesSelect.setAttribute("placeholder", "Assignee");
+  assigneesSelect.setAttribute("multiple", true);
+  formContainer.appendChild(assigneesSelect);
+  for (let assignee of assignees) {
+    let option = document.createElement("option");
+    option.value = assignee;
+    option.innerHTML = assignee;
+    assigneesSelect.appendChild(option);
   }
+
+  let addButton = document.createElement("button");
+  addButton.innerHTML = "Add";
+  addButton.setAttribute("type", "submit");
+  addButton.setAttribute("id", "add-btn");
+  formContainer.appendChild(addButton);
+}
 
 function onFormSubmit(event) {
-    event.preventDefault();
-    let title = document.getElementById("form-title").value;
-    let description = document.getElementById("form-description").value;
-    let assignees = document.getElementById("form-assignees").options;
-    let selectedAssignees = [];
-    for (let assignee of assignees) {
-      if(assignee.selected === true) {
-        selectedAssignees.push(assignee.value);}
+  event.preventDefault();
+  let title = document.getElementById("form-title").value;
+  let description = document.getElementById("form-description").value;
+  let assignees = document.getElementById("form-assignees").options;
+  let selectedAssignees = [];
+  for (let assignee of assignees) {
+    if (assignee.selected === true) {
+      selectedAssignees.push(assignee.value);
     }
-    let task = {
-        id: makeid(10),
-        title: title,
-        description: description,
-        assignees: selectedAssignees,
-        state: "not_started",
-        blockReason: "",
-        priority: 1,
-    }
-    
-    addTask(task);
-    taskList.push(task);
   }
+  let task = {
+    id: makeid(10),
+    title: title,
+    description: description,
+    assignees: selectedAssignees,
+    state: "not_started",
+    blockReason: "",
+    priority: 1,
+  };
+
+  addTask(task);
+  taskList.push(task);
+  let inputFields = document.getElementsByClassName("form-input");
+  for (let inputField of inputFields) {
+    inputField.value = "";
+  }
+}
 
 function dragTask(event) {
-      event.dataTransfer.setData("task-id", event.target.id);
-      event.dataTransfer.setData("task-state", event.target.parentNode.id);
-  }
+  event.dataTransfer.setData("task-id", event.target.id);
+  event.dataTransfer.setData("task-state", event.target.parentNode.id);
+}
 
 function allowDropTask(event) {
   event.preventDefault();
@@ -171,31 +173,31 @@ function allowDropTask(event) {
 
 function dropTask(event) {
   event.preventDefault();
-    let taskId = event.dataTransfer.getData("task-id");
-    let taskDiv = document.getElementById(taskId);
-    let taskState = event.dataTransfer.getData("task-state");
-    let pileDiv = event.currentTarget;
+  let taskId = event.dataTransfer.getData("task-id");
+  let taskDiv = document.getElementById(taskId);
+  let taskState = event.dataTransfer.getData("task-state");
+  let pileDiv = event.currentTarget;
 
-    taskDiv.classList.remove(taskState);
-    taskDiv.classList.add(pileDiv.id);
-    
-    for(let task of taskList) {
-      if(task.id === taskId) {
-        task.state = pileDiv.id;
-      }
+  taskDiv.classList.remove(taskState);
+  taskDiv.classList.add(pileDiv.id);
+
+  for (let task of taskList) {
+    if (task.id === taskId) {
+      task.state = pileDiv.id;
     }
+  }
 
-    pileDiv.appendChild(taskDiv);
-
+  pileDiv.appendChild(taskDiv);
 }
 
 function clearInputValue(event) {
-    event.target.value = "";
+  event.target.value = "";
 }
 
 function makeid(length) {
-  let result = '';
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   const charactersLength = characters.length;
   let counter = 0;
   while (counter < length) {
@@ -205,19 +207,3 @@ function makeid(length) {
   return result;
 }
 
-
-{/* <div id="task-form-container">
-<input type="text" id="task-title" value="Title">
-<input type=" text" id="task-description" value="Description">  
-<input type="text" id="task-assignees" value="Assignee">
-<button type="submit">Add</button>
-</div> */}
-
-
-{/* <div id="not_started">
-<div class="task not_started">
-    <p>Title: task.title</p>
-    <p>Description: task.description</p>
-    <p>Assignees: task.assignees[0]</p>
-</div>
-</div> */}
